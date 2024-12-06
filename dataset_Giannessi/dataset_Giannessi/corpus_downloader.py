a2d9e1247f69097643f6abf6a56be0eb8a3abb41
Gabriele <gabrielegiannessi@gmail.com>
2024-12-06 15:16:35 +0100

    Prova

--- dataset_Giannessi/dataset_Giannessi/corpus_downloader.py
+++
@@ -1,8 +0,0 @@
-from cltk.data.fetch import FetchCorpus
-
-corpus_downloader = FetchCorpus(language="grc")
-
-for corpus in corpus_downloader._get_user_defined_corpora():
-    corpus_downloader.import_corpus(corpus_name=corpus['name'])
-
-
