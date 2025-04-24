472f899bf797395edc2af596d9aa2537ebf564a4
Gabriele <gabrielegiannessi@gmail.com>
2025-04-24 16:03:32 +0200

    Aggiunto `num_tokens` come campo nel frontend

--- frontend/src/app/components/suggests-box/suggests-box.component.ts
+++ frontend/src/app/components/suggests-box/suggests-box.component.ts
@@ -1,36 +1,48 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { Component, computed, effect, inject, input, signal } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, FormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { ApiService, modelType, SuggestionInterface } from '../../services/api.service';
 import { ModelComponent } from '../model/model.component';
 import { SuggestComponent } from '../suggest/suggest.component';
 
 @Component({
   selector: 'app-suggests-box',
-  imports: [ReactiveFormsModule, ModelComponent, SuggestComponent],
+  imports: [ReactiveFormsModule, FormsModule, ModelComponent, SuggestComponent],
   templateUrl: './suggests-box.component.html',
   styleUrl: './suggests-box.component.css'
 })
 export class SuggestsBoxComponent {
-
   api = inject(ApiService);
   models = input.required<modelType[]>()
+  form: FormGroup;
 
   suggestions = signal<SuggestionInterface[]>([]);
+  curr_id = signal<string | null>(null);
+  selectedModel = computed(() => {
+    return this.curr_id() ? this.models().find((model) => model._id === this.curr_id()) || null : null;
+  });
+  curr_type_model = computed(() => this.selectedModel()?.TYPE)
 
-  debug = effect(() => {console.log('suggestions:', this.suggestions())});
+  constructor() {
+    this.form = new FormGroup({
+      text: new FormControl<string>('', { validators: [Validators.required, this.isContextValid, Validators.minLength(10)] }),
+      modelID: new FormControl<string>('', {
+        validators: [Validators.required, Validators.minLength(24),
+        Validators.maxLength(24)]
+      }),
+      num_tokens: new FormControl<number>(1, { validators: [Validators.required, Validators.min(1), Validators.max(10)] })
+    })
+  }
+
+  setCurrentID($event: Event) {
+    const target = $event.target as HTMLInputElement;
+    const modelID = target.value;
+    this.curr_id.set(modelID);
+  }
 
   isContextValid = (c: AbstractControl): ValidationErrors | null => {
     if (!c.value) return { notvalid: true };
     return c.value.search("[...]") !== -1 ? null : { notMasked: true };
   }
-
-  form = new FormGroup({
-    text: new FormControl<string>('', { validators: [Validators.required, this.isContextValid, Validators.minLength(10)] }),
-    modelID: new FormControl<string>('', {
-      validators: [Validators.required, Validators.minLength(24),
-      Validators.maxLength(24)]
-    }),
-  })
 
   toggleModels() {
     const models = document.querySelectorAll('#modelsCard');
@@ -76,51 +88,58 @@ export class SuggestsBoxComponent {
       setTimeout(() => alert.remove(), 150);
     }, 5000);
   }
-
   generateSuggestions() {
     this.form.markAllAsTouched(); // forza la validazione
     this.form.updateValueAndValidity(); // aggiorna tutti i validator
-
-    
-
+  
     if (this.form.invalid) {
       const textErrors = this.form.controls['text'].errors;
       const modelErrors = this.form.controls['modelID'].errors;
-
+      const tokenErrors = this.form.controls['num_tokens'].errors;
+  
       if (textErrors?.['required']) {
         this.showAlert('Il campo testo è obbligatorio', 'danger');
         return;
       }
-
+  
       if (textErrors?.['notMasked']) {
         this.showAlert('Il testo non contiene la lacuna [...]', 'warning');
         return;
       }
-
+  
       if (textErrors?.['minlength']) {
         this.showAlert('Il testo è troppo corto', 'danger');
         return;
       }
-
+  
       if (modelErrors?.['required']) {
         this.showAlert('Seleziona un modello', 'danger');
         return;
       }
-
+  
       if (modelErrors?.['minlength'] || modelErrors?.['maxlength']) {
         this.showAlert('L\'ID del modello deve essere di 24 caratteri', 'danger');
         return;
       }
-
+  
+      if (tokenErrors?.['required']) {
+        this.showAlert('Specifica il numero di token', 'danger');
+        return;
+      }
+  
+      if (tokenErrors?.['min'] || tokenErrors?.['max']) {
+        this.showAlert('Il numero di token deve essere tra 1 e 10', 'danger');
+        return;
+      }
+  
       return;
     }
-
-    const { text, modelID } = this.form.getRawValue();
-
+  
+    const { text, modelID, num_tokens } = this.form.getRawValue();
+  
     this.toggleSpinner();
-
-    // default di num_tokens a 1
-    this.api.generateSuggestion(modelID!, text!, 1).subscribe({
+  
+    this.api.generateSuggestion(modelID!, text!, num_tokens!).subscribe({
       next: (response) => {
         this.suggestions.set(response);
         this.showAlert('Suggerimenti generati con successo', 'success');
