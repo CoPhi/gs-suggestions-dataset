0d29f3b1c7cc3766d0abaa06b94ea34aa3983cbc
Gabriele <gabrielegiannessi@gmail.com>
2025-05-14 14:42:37 +0200

    fix attach db to API

--- api/database.py
+++ api/database.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
-client = MongoClient(f"mongodb://{ username }:{ password }@localhost:27017/")
+host = os.getenv("MONGO_HOST", "localhost") #di default prende come valore `localhost`
+client = MongoClient(f"mongodb://{ username }:{ password }@{ host }:27017/")
 db = client.models_db   
 collection = db['models_collection']
 fs = GridFS(db)
