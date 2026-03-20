86aa445e17533fb78ca7ecdaacb26a52418d87fa
Gabriele <gabrielegiannessi@gmail.com>
2026-03-20 17:00:26 +0100

    deleting informations from code

--- finetuning/dataset.py
+++ finetuning/dataset.py
@@ -1,11 +1,5 @@
 """
 Script per rendere disponibile l'insieme delle frasi pre-processsate del MAAT corpus su hugging face
-
-access token: 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
-nome: hf-maat-upload
-
-token dell'organizzazione CNR-ILC: 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
-
 Questo dataset serve per fare il finetuning dei modelli BERT per il greco antico sul task MLM
 
 Il train_set è composto da frasi pulite in greco antico senza la presenza di token sconosciuti, in cui è stato applicato il case folding.
@@ -21,13 +15,18 @@ from finetuning import (
 )
 from finetuning.utils import get_filtered_processed_sentences
 from train import load_test_abs, get_sentences
-from finetuning import TRAIN_DATASET_CHECKPOINT, TEST_DATASET_CHECKPOINT, get_cast_unk_tokens_text, get_sent_from_tokens
+from finetuning import (
+    TRAIN_DATASET_CHECKPOINT,
+    TEST_DATASET_CHECKPOINT,
+    get_cast_unk_tokens_text,
+    get_sent_from_tokens,
+)
 from nltk.lm.preprocessing import flatten
 
 from train.cleaner import load_abs, load_specific_domain_abs, split_abs
 
-def push_trainset_to_huggingface_hub(dataset: DatasetDict, message: str) -> None:
 
+def push_trainset_to_huggingface_hub(dataset: DatasetDict, message: str) -> None:
     dataset.push_to_hub(TRAIN_DATASET_CHECKPOINT, commit_message=message)
 
 
@@ -40,23 +39,32 @@ def push_set_to_huggingface_hub(dataset: DatasetDict, message: str) -> None:
 
 
 def main():
-
     train_abs, dev_abs = load_and_split_sentences()
     test_abs = load_test_abs()
     dataset = DatasetDict(
         {
             "train": get_processed_sentences(train_abs),
             "dev": get_processed_sentences(dev_abs),
-            "test": Dataset.from_dict({
-                "text": [get_cast_unk_tokens_text(get_sent_from_tokens(sent_tkns)) for sent_tkns in get_sentences(test_abs, case_folding=True, remove_punct=True)],
-                })
+            "test": Dataset.from_dict(
+                {
+                    "text": [
+                        get_cast_unk_tokens_text(get_sent_from_tokens(sent_tkns))
+                        for sent_tkns in get_sentences(
+                            test_abs, case_folding=True, remove_punct=True
+                        )
+                    ],
+                }
+            ),
         }
     )
 
-    push_set_to_huggingface_hub(dataset, "Unlabeled ancient greek sentences for fill-mask task, folded to uppercase, remove punct, with dev & test set")
+    push_set_to_huggingface_hub(
+        dataset,
+        "Unlabeled ancient greek sentences for fill-mask task, folded to uppercase, remove punct, with dev & test set",
+    )
+
 
 if __name__ == "__main__":
-    #main()
     train_abs = load_abs()
     domain_abs = load_specific_domain_abs(abs=train_abs)
     train_domain_abs, dev_domain_abs = split_abs(domain_abs, test_size=0.2)
