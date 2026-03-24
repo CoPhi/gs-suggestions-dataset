ae0a3af63918bb472f41e45e7828582145820dd8
Gabriele <gabrielegiannessi@gmail.com>
2026-03-24 10:47:56 +0100

    update description

--- api/main.py
+++ api/main.py
@@ -19,10 +19,7 @@ def get_version() -> str:
 app = FastAPI(
     title="gs-api",
     version=get_version(),
-    description="""API per GreekSchools. Questa API è progettata per offrire l'accesso a modelli linguistici basati su n-grammi e BERT per la generazione di supplementi testuali. 
-    Il suo scopo principale è quello di riempire le lacune presenti nei testi, 
-    in particolare nei testi antichi o incompleti, suggerendo parole o frasi che possono essere utilizzate per completare i passaggi mancanti. """,
-)
+    description="""API per GreekSchools. Questa API è progettata per offrire l'accesso a modelli linguistici basati su n-grammi e BERT per la generazione di supplementi testuali. """)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
