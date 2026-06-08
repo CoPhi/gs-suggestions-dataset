e6f22b528d0f777f8d452a2638145eb7c3ff6ae3
¨Gabriele <gabrielegiannessi@gmail.com>
2026-06-08 18:25:22 +0200

    feat: add project root to sys.path to resolve module imports in opt.py

--- scripts/opt.py
+++ scripts/opt.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import wandb
 import argparse
 from models.bert.finetuning.pipeline import pipeline_finetuning
